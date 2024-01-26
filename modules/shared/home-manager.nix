09cfb95f54064c4453b2b109dbda51f2fe3fca36
Oleh Khalin <oleghalinua@gmail.com>
2024-01-26 10:29:19 +0100

    fix: github identity

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -182,23 +182,6 @@ let name = "Oleh Khalin";
 
   ssh = {
     enable = true;
-    
-
-    extraConfig = lib.mkMerge [
-      ''
-        Host github.com
-          Hostname github.com
-          IdentitiesOnly yes
-      ''
-      (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
-        ''
-          IdentityFile /home/${user}/.ssh/id_github
-        '')
-      (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
-        ''
-          IdentityFile /Users/${user}/.ssh/id_github
-        '')
-    ];
   };
 
   tmux = {
